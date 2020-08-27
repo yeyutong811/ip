82be298ccdff402f96d3e1e1ec92b1599d95c8a2
YeYutong <E0425083@u.nus.edu>
2020-08-28 01:06:21 +0800

    Added Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,16 +10,34 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
+        repeatCommand();
     }
 
     public static void greet(){
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println(" ");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+    }
 
+    public static void bye(){
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void repeatCommand(){
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        while (line.equals("bye") == false) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + line);
+            System.out.println("    ____________________________________________________________");
+            line = in.nextLine();
+        }
+
+        bye();
     }
 }
